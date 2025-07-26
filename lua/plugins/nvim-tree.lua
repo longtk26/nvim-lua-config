a88beca035b3935c545e59f778b2e737de332f93
Pham Ngoc Long <ngoclong2663@gmail.com>
2025-07-26 17:54:57 +0700

    feat: update nvim-tree configuration to include filter settings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,6 +6,23 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      filters = {
+        dotfiles = false,  -- Set to false to show dotfiles like .env
+        git_clean = false,
+        no_buffer = false,
+        custom = {
+          -- You can add specific files to hide here if needed
+          -- "^.git$",
+        },
+        exclude = {
+          -- Files you always want to show even if they match other filters
+          ".env",
+          ".env.local",
+          ".env.example",
+          ".gitignore",
+        },
+      },
+    }
   end,
 }
